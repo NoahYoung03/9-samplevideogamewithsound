8dfcceab90f323007b4237a2b5fcd35bf4d99962
NoahYoung03 <32547208+NoahYoung03@users.noreply.github.com>
2017-11-17 11:07:32 -0500

    Add files via upload

---
+++ splash_screen.lua
@@ -0,0 +1,167 @@
+-----------------------------------------------------------------------------------------
+--
+-- splash_screen.lua
+-- Created by: Your Name
+-- Date: Month Day, Year
+-- Description: This is the splash screen of the game. It displays the 
+-- company logo that...
+-----------------------------------------------------------------------------------------
+
+-- Use Composer Library
+local composer = require( "composer" )
+
+-- Name the Scene
+sceneName = "splash_screen"
+
+-----------------------------------------------------------------------------------------
+
+-- Create Scene Object
+local scene = composer.newScene( sceneName )
+
+----------------------------------------------------------------------------------------
+-- LOCAL VARIABLES
+-----------------------------------------------------------------------------------------
+ 
+-- The local variables for this scene
+local soccerball
+local scrollXSpeed = 8
+local scrollYSpeed = -3
+local jungleSounds = audio.loadSound("Sounds/animals144.mp3")
+local jungleSoundsChannel
+local bkg2
+
+--------------------------------------------------------------------------------------------
+-- LOCAL FUNCTIONS
+--------------------------------------------------------------------------------------------
+display.setStatusBar(display.HiddenStatusBar)
+
+-- The function that moves the soccerball across the screen
+local function movesoccerball()
+    soccerball.x = soccerball.x + scrollXSpeed
+    soccerball.y = soccerball.y + scrollYSpeed
+end
+
+-- The function that will go to the main menu 
+local function gotoMainMenu()
+    composer.gotoScene( "main_menu" )
+end
+
+-----------------------------------------------------------------------------------------
+-- GLOBAL SCENE FUNCTIONS
+-----------------------------------------------------------------------------------------
+  
+-- The function called when the screen doesn't exist
+function scene:create( event )
+
+    -- Creating a group that associates objects with the scene
+    local sceneGroup = self.view
+    
+     bkg2 = display.newImageRect("Images/CompanyLogo.png", 1080, 768)
+     bkg2.x = display.contentWidth/2 
+     bkg2.y = display.contentHeight/2 
+  
+
+     -- Insert the soccerball image
+     soccerball = display.newImageRect("Images/soccerball.png", 200, 200)
+     -- set the initial x and y position of the soccerball
+     soccerball.x = 100
+     soccerball.y = display.contentHeight/2
+
+     -- Insert the soccerball image
+
+     -- Insert objects into the scene group in order to ONLY be associated with this scene
+     sceneGroup:insert( bkg2 )
+     sceneGroup:insert( soccerball )
+
+end -- function scene:create( event )
+
+--------------------------------------------------------------------------------------------
+
+-- The function called when the scene is issued to appear on screen
+function scene:show( event )
+
+    -- Creating a group that associates objects with the scene
+    local sceneGroup = self.view
+
+    -----------------------------------------------------------------------------------------
+
+    local phase = event.phase
+
+    -----------------------------------------------------------------------------------------
+
+    -- Called when the scene is still off screen (but is about to come on screen).
+    if ( phase == "will" ) then
+       
+    -----------------------------------------------------------------------------------------
+
+    elseif ( phase == "did" ) then
+        -- start the splash screen music
+        jungleSoundsChannel = audio.play(jungleSounds )
+
+        -- Call the movesoccerball function as soon as we enter the frame.
+        Runtime:addEventListener("enterFrame", movesoccerball)
+
+        -- Go to the main menu screen after the given time.
+        timer.performWithDelay ( 3000, gotoMainMenu)          
+        
+    end
+
+end --function scene:show( event )
+
+-----------------------------------------------------------------------------------------
+
+-- The function called when the scene is issued to leave the screen
+function scene:hide( event )
+
+    -- Creating a group that associates objects with the scene
+    local sceneGroup = self.view
+    local phase = event.phase
+    bkg2.isVisible = false
+    soccerball.isVisible = false
+    -----------------------------------------------------------------------------------------
+
+    -- Called when the scene is on screen (but is about to go off screen).
+    -- Insert code here to "pause" the scene.
+    -- Example: stop timers, stop animation, stop audio, etc.
+    if ( phase == "will" ) then  
+
+    -----------------------------------------------------------------------------------------
+
+    -- Called immediately after scene goes off screen.
+    elseif ( phase == "did" ) then
+        
+        -- stop the jungle sounds channel for this screen
+        audio.stop(jungleSoundsChannel)
+    end
+
+end --function scene:hide( event )
+
+-----------------------------------------------------------------------------------------
+
+-- The function called when the scene is issued to be destroyed
+function scene:destroy( event )
+
+    -- Creating a group that associates objects with the scene
+    local sceneGroup = self.view
+
+    -----------------------------------------------------------------------------------------
+
+
+    -- Called prior to the removal of scene's view ("sceneGroup").
+    -- Insert code here to clean up the scene.
+    -- Example: remove display objects, save state, etc.
+end -- function scene:destroy( event )
+
+-----------------------------------------------------------------------------------------
+-- EVENT LISTENERS
+-----------------------------------------------------------------------------------------
+
+-- Adding Event Listeners
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
+
+-----------------------------------------------------------------------------------------
+
+return scene
